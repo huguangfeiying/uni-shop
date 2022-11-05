3b9ceba340f861db9a84ecb8ffb1af68645a545f
huguangfeiying <2671252974@qq.com>
2022-11-05 22:28:40 +0800

    完成购物车功能

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -23,7 +23,7 @@
         </view>
       </view>
       <!-- 运费 -->
-      <view class="yf">快递：免运费</view>
+      <view class="yf">快递：免运费 {{cart.length}}</view>
     </view>
 
     <!-- 商品详情信息 -->
@@ -44,6 +44,7 @@
 </template>
 
 <script>
+  import { mapState, mapMutations, mapGetters } from 'vuex'
   export default {
     data() {
       return {
@@ -56,8 +57,8 @@
         }, {
           icon: 'cart',
           text: '购物车',
-          info: 2
-        }],
+          info: 0
+        }, ],
         // 右侧按钮组的配置对象
         buttonGroup: [{
             text: '加入购物车',
@@ -72,6 +73,12 @@
         ]
       };
     },
+    computed: {
+      ...mapState({
+        cart: state => state.cart.cart
+      }),
+      ...mapGetters(['total'])
+    },
     onLoad(options) {
       // 获取商品 Id
       const goods_id = options.goods_id
@@ -79,6 +86,7 @@
       this.getGoodsDetail(goods_id)
     },
     methods: {
+      ...mapMutations(['addToCart']),
       // 定义请求商品详情数据的方法
       async getGoodsDetail(goods_id) {
         const { data: res } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id })
@@ -107,8 +115,39 @@
             url: '/pages/cart/cart'
           })
         }
+      },
+      // 右侧按钮的点击事件处理函数
+      buttonClick(e) {
+        // 1. 判断是否点击了 加入购物车 按钮
+        if (e.content.text === '加入购物车') {
+          // 2. 组织一个商品的信息对象
+          const goods = {
+            goods_id: this.goods_info.goods_id, // 商品的Id
+            goods_name: this.goods_info.goods_name, // 商品的名称
+            goods_price: this.goods_info.goods_price, // 商品的价格
+            goods_count: 1, // 商品的数量
+            goods_small_logo: this.goods_info.goods_small_logo, // 商品的图片
+            goods_state: true // 商品的勾选状态
+          }
+          // 3. 通过 this 调用映射过来的 addToCart 方法，把商品信息对象存储到购物车中
+          this.addToCart(goods)
+        }
       }
-    }
+    },
+    watch: {
+      // 1. 监听 total 值的变化，通过第一个形参得到变化后的新值
+      total: {
+        handler(newVal) {
+          // 2. 通过数组的 find() 方法，找到购物车按钮的配置对象
+          const findResult = this.options.find((x) => x.text === '购物车')
+          if (findResult) {
+            // 3. 动态为购物车按钮的 info 属性赋值
+            findResult.info = newVal
+          }
+        },
+        immediate: true
+      }
+    },
   }
 </script>
 
