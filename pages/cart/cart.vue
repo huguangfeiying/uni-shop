80a37698fbf2af64f8634887097618668fc65230
huguangfeiying <2671252974@qq.com>
2022-11-06 15:10:36 +0800

    完成购物车结算功能

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <view>
+  <view class="cart-container" v-if="cart.length !== 0">
     <!--收货地址组件-->
     <my-address></my-address>
     <!-- 购物车商品列表的标题区域 -->
@@ -24,6 +24,12 @@
 
     <!-- 结算区域 -->
     <my-settle></my-settle>
+  </view>
+
+  <!-- 空白购物车区域 -->
+  <view class="empty-cart" v-else>
+    <image src="/static/cart_empty@2x.png" class="empty-img"></image>
+    <text class="tip-text">空空如也~</text>
   </view>
 </template>
 
@@ -69,16 +75,38 @@
 </script>
 
 <style lang="scss">
-  .cart-title {
-    height: 40px;
-    display: flex;
-    align-items: center;
-    padding-left: 5px;
-    border-bottom: 1px solid #efefef;
+  .cart-container {
+    padding-bottom: 50px;
 
-    .cart-title-text {
-      font-size: 14px;
-      margin-left: 10px;
+    .cart-title {
+      height: 40px;
+      display: flex;
+      align-items: center;
+      padding-left: 5px;
+      border-bottom: 1px solid #efefef;
+
+      .cart-title-text {
+        font-size: 14px;
+        margin-left: 10px;
+      }
+    }
+  }
+
+  .empty-cart {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 150px;
+
+    .empty-img {
+      width: 90px;
+      height: 90px;
+    }
+
+    .tip-text {
+      font-size: 12px;
+      color: gray;
+      margin-top: 15px;
     }
   }
 </style>
